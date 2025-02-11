88638aad36bc096ca5f0be3c46fae6f4d926a875
Maredmc <63470707+Maredmc@users.noreply.github.com>
2025-02-11 13:28:54 +0100

    Update BedModel.tsx

--- app/components/BedModel.tsx
+++ app/components/BedModel.tsx
@@ -2,7 +2,7 @@
 
 import { useRef } from "react"
 import { useFrame, useLoader } from "@react-three/fiber"
-import { Box, Text } from "@react-three/drei"
+import { Text } from "@react-three/drei"
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
 interface BedModelProps {
@@ -15,43 +15,33 @@ interface BedModelProps {
 
 export default function BedModel({ size, sideRails, evolutionKit, color, showDimensions }: BedModelProps) {
   const bedRef = useRef()
-  const obj = useLoader(OBJLoader, "/EC19080.obj");
+  const obj = useLoader(OBJLoader, "/models/EC19080.obj");
 
   useFrame((state, delta) => {
-    bedRef.current.rotation.y += delta * 0.2
+    if (bedRef.current) {
+      bedRef.current.rotation.y += delta * 0.2;
+    }
   })
-
-  const bedDimensions = {
-    single: [1.9, 0.8],
-    reduced_single: [1.6, 0.8],
-    one_and_half: [1.9, 1.2],
-  }
-
-  const [length, width] = bedDimensions[size] || bedDimensions.single
-  const height = evolutionKit === "large" ? 0.23 : evolutionKit === "small" ? 0.11 : 0.05
-  const bedColor = color === "white" ? "#FFFFFF" : "#D4B78F"
 
   return (
     <group ref={bedRef}>
-      <primitive object={obj} scale={1} />
-
+      <primitive object={obj} scale={[1, 1, 1]} />
       {/* Dimensions */}
       {showDimensions && (
         <>
-          <Text position={[0, -0.1, length / 2 + 0.1]} rotation={[-Math.PI / 2, 0, 0]} fontSize={0.1} color="black">
-            {`${length * 100} cm`}
+          <Text position={[0, -0.1, 1]} rotation={[-Math.PI / 2, 0, 0]} fontSize={0.1} color="black">
+            {"190 cm"}
           </Text>
           <Text
-            position={[width / 2 + 0.1, -0.1, 0]}
+            position={[0.5, -0.1, 0]}
             rotation={[-Math.PI / 2, 0, Math.PI / 2]}
             fontSize={0.1}
             color="black"
           >
-            {`${width * 100} cm`}
+            {"80 cm"}
           </Text>
         </>
       )}
     </group>
   )
 }
-
