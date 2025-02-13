cadc01a60c88424fdd5a4e2268ab7cf94643f04a
Maredmc <63470707+Maredmc@users.noreply.github.com>
2025-02-13 17:51:43 +0100

    Update BedModel.tsx

--- app/components/BedModel.tsx
+++ app/components/BedModel.tsx
@@ -41,7 +41,7 @@ export default function BedModel({ selectedAddon }: BedModelProps) {
         <primitive object={gltfPiedini.scene} position={[-2, bedBaseY, -0.5]} />
       )}
       {selectedAddon === "piedone" && (
-        <primitive object={gltfPiedone.scene} position={[-3.65, bedBaseY, -0.95]} />
+        <primitive object={gltfPiedone.scene} position={[-3.62, bedBaseY, -0.92]} />
       )}
     </group>
   );
